6b5105bb6258a92edf3d0e0aa543953daf9ad158
Mariana Torres <marith.3289@gmail.com>
2024-11-14 18:32:29 -0600

    Actualización del proyecto

--- js/script.js
+++ js/script.js
@@ -1,8 +1,18 @@
 document.getElementById('newsletterForm').addEventListener('submit', function(event) {
-    event.preventDefault();
+    event.preventDefault(); // Evita el envío del formulario
+    
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
     console.log(`Nombre: ${name}, Email: ${email}`);
-    alert("Felicidades, te has registrado con éxito. Ahora recibirás nuestras recetas primero.");
+    
+    // Muestra el modal de éxito de Bootstrap
+    const successModal = new bootstrap.Modal(document.getElementById('successModal'));
+    successModal.show();
+    
+    // Oculta el modal automáticamente después de 3 segundos
+    setTimeout(() => {
+        successModal.hide();
+    }, 5000);
+    
     this.reset(); // Limpia el formulario
 });
